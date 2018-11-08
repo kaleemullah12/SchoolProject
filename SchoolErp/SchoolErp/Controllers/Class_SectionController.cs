c9e8857e9b8725232ba61c1796cb546e772a314a
kaleemullah12 <37169000+kaleemullah12@users.noreply.github.com>
2018-11-08 02:54:31 -0800

    as

---
+++ SchoolErp/SchoolErp/Controllers/Class_SectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolErp.Controllers
+{
+    public class Class_SectionController : Controller
+    {
+        // GET: Class_Section
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
